ccf46fb4f10e37d11bc9a39c3aa89a25e64460fa
luis2ra <luis2ra@gmail.com>
2021-03-03 23:35:39 -0500

    Creación de las apps y los modelos respectivos

---
+++ simple_blog/categories/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """Category model."""
+    name = models.CharField(max_length=100,unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
